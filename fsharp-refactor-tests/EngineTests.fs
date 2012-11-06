39acfcafce446c382cabb094268defdd1e4ea7a4
Lewis Brown <lewix7@hotmail.com>
2012-11-06 19:11:21 +0000

    Add a function to parse code

--- fsharp-refactor-tests/EngineTests.fs
+++ fsharp-refactor-tests/EngineTests.fs
@@ -1,12 +1,21 @@
 namespace FSharpRefactor.Tests.EngineTests
 
-open System.Collections
 open NUnit.Framework
+open Microsoft.FSharp.Compiler.Ast
 
 open FSharpRefactor.Engine
 
 [<TestFixture>]
 type ASTFetcherModule() =
     [<Test>]
-    member this.ThereIsAParseTree() =
+    member this.``There is a parse tree``() =
         Assert.IsTrue(ASTFetcher.parseTree.IsSome)
+
+    [<Test>]
+    member this.``Parsing some simple code is successful``() =
+        let expected t =
+            match t with
+                | Some(ParsedInput.ImplFile(ParsedImplFileInput(_,_,_,_,_,_,_))) -> true
+                | _ -> false
+        Assert.IsTrue(expected (ASTFetcher.Parse "let a = 1"))
+    
--- fsharp-refactor/astfetch.fs
+++ fsharp-refactor/astfetch.fs
@@ -7,12 +7,8 @@ module ASTFetcher =
     let checker = InteractiveChecker.Create(NotifyFileTypeCheckStateIsDirty(fun _ -> ()))
     let filename = "/home/lewis/test.fs"
     let source = "let a = 1"
-    let options = {ProjectFileName = "/mnt/media/git/university/personalproj/fsharp-refactor/fsharp-refactor/fsharp-refactor.fsproj"
-                   ProjectFileNames = [|""|]
-                   ProjectOptions = [||]
-                   IsIncompleteTypeCheckEnvironment = true
-                   UseScriptResolutionRules = false
-                   LoadTime = System.DateTime.Now
-                   UnresolvedReferences = None}
+    let options = checker.GetCheckOptionsFromScriptRoot(filename, source, DateTime.Now, [| |])
     let untypedParseInfo = checker.UntypedParse(filename, source, options)
     let parseTree = untypedParseInfo.ParseTree
+    
+    let Parse source = checker.UntypedParse(filename, source, options).ParseTree
