a731b9db4a19324d0e8dd9e8e19966a4832e4600
Lewis Brown <lewix7@hotmail.com>
2012-11-08 16:34:04 +0000

    Implement an, incomplete, Height function

--- fsharp-refactor/astfetch.fs
+++ fsharp-refactor/astfetch.fs
@@ -11,4 +11,38 @@ module ASTFetcher =
     
     let Parse source = checker.UntypedParse(filename, source, options source).ParseTree
 
-    let Height tree = 0
+    let maxOfList ns = Seq.fold max 0 ns
+
+    let rec patternHeight pattern =
+        match pattern with
+            | SynPat.Named(innerPattern,_,_,_,_) -> 1 + (patternHeight innerPattern)
+            | SynPat.Wild(_) -> 0
+            | _ -> raise (new NotImplementedException("This declaration is not supported"))
+            
+    let expressionHeight expression =
+        match expression with
+            | SynExpr.Const(_,_) -> 1
+            | _ -> raise (new NotImplementedException("This declaration is not supported"))
+            
+    let bindingHeight (Binding(_,_,_,_,_,_,_,pattern,_,expression,_,_)) =
+        1 + (maxOfList [(patternHeight pattern); (expressionHeight expression)])
+    
+    let declarationHeight declaration =
+        match declaration with
+            | SynModuleDecl.Let(_,bindings,_) ->
+                let bindingHeights = Seq.map bindingHeight bindings
+                1 + (maxOfList bindingHeights)
+            | _ -> raise (new NotImplementedException("This declaration is not supported"))
+        
+    let moduleOrNamespaceHeight (SynModuleOrNamespace(_,_,declarations,_,_,_,_)) =
+        let declarationHeights = Seq.map declarationHeight declarations
+        1 + (maxOfList declarationHeights)
+
+    let Height tree =
+        match tree with
+            | ParsedInput.ImplFile(ParsedImplFileInput(_,_,_,_,_,modulesOrNamespaces,_)) ->
+                let moduleHeights = Seq.map moduleOrNamespaceHeight modulesOrNamespaces
+                maxOfList moduleHeights
+            | _ -> raise (new NotImplementedException("Use an impl file instead"))
+
+    
