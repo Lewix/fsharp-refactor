f6d37774152a6b39b67cb351884d6704a9ae0c91
Lewis Brown <lewix7@hotmail.com>
2012-11-14 17:57:27 +0000

    Implement AddChild

--- fsharp-refactor/Ast.fs
+++ fsharp-refactor/Ast.fs
@@ -47,6 +47,12 @@ module Ast =
             | Binding(b) ->
                 match b with
                     | SynBinding.Binding(_,_,_,_,_,_,_,p,_,e,_,_) -> Some([AstNode.Pattern p; AstNode.Expression e])
+
+            | Expression(e) ->
+                match e with
+                    | SynExpr.LetOrUse(_,_,bs,e,_) ->  Some(List.append (List.map AstNode.Binding bs) [AstNode.Expression e])
+                    | _ -> raise (new NotImplementedException("Add a new entry to pattern for Expression: " + (string e)))
+                    
             | _ -> raise (new NotImplementedException("Add a new entry to the active pattern for Children:" + (string node)))
 
     let (|Range|_|) (node : AstNode) =
@@ -54,6 +60,9 @@ module Ast =
             | File _ -> None
             | Pattern p -> Some(p.Range)
             | ModuleOrNamespace ns -> Some(ns.Range)
+            | Module m -> Some(m.Range)
+            | Binding b -> Some(b.RangeOfBindingAndRhs)
+            | Expression e -> Some(e.Range)
             | _ -> raise (new NotImplementedException("Add a new entry to the active pattern for Range:" + (string node)))
 
     // Utility functions to avoid having to match patterns to get children or range
--- fsharp-refactor/TreeTransforms.fs
+++ fsharp-refactor/TreeTransforms.fs
@@ -6,45 +6,52 @@ open FSharpRefactor.Engine.Ast
 
 module TreeTransforms =
     exception InvalidRange
-    
-    let ChangeTextOf (source : string) (nodeTextPairsToChange : (Ast.AstNode * string) list) =
-        // Expect the nodes to have a range. If not, raise an exception
-        let getRange node =
-            let range = Ast.GetRange node
-            if range.IsSome then range.Value
-            else raise InvalidRange
+    exception InvalidNode
 
+    let replaceRange (source : string) (range : range, replacementText : string) =
+        let notLineSep = fun c -> c <> '\n'
+        let rec takeAroundPos before after (line, column) =
+            // Lines are indexed from 1, columns from 0
+            match (line, column) with
+                | 1,c -> (Seq.append before (Seq.take column after), Seq.skip column after)
+                | n,_ ->
+                    takeAroundPos (Seq.concat [before; (Seq.takeWhile notLineSep after); seq['\n']])
+                                  (Seq.skip 1 (Seq.skipWhile notLineSep after))
+                                  (line-1, column)
+
+        let before, _ = takeAroundPos "" source (range.StartLine, range.StartColumn)
+        let _, after = takeAroundPos "" source (range.EndLine, range.EndColumn)
+
+        (Seq.fold (+) "" (Seq.map string before)) + replacementText + (Seq.fold (+) "" (Seq.map string after))
+
+    // Expect the nodes to have a range. If not, raise an exception
+    let getRange node =
+        let range = Ast.GetRange node
+        if range.IsSome then range.Value
+        else raise InvalidRange
+
+
+    let ChangeTextOf (source : string) (nodeTextPairsToChange : (Ast.AstNode * string) list) =
         let sortedPairs =
             let sortFunction (node1, _) (node2, _) =
                 -rangeOrder.Compare(getRange node1, getRange node2)
 
             List.sortWith sortFunction nodeTextPairsToChange
 
-        let replaceOne (source : string) (node, replacementText) =
-            let notLineSep = fun c -> c <> '\n'
-            let rec takeAroundPos before after (line, column) =
-                // Lines are indexed from 1, columns from 0
-                match (line, column) with
-                    | 1,c -> (Seq.append before (Seq.take column after), Seq.skip column after)
-                    | n,_ ->
-                        takeAroundPos (Seq.concat [before; (Seq.takeWhile notLineSep after); seq['\n']])
-                                      (Seq.skip 1 (Seq.skipWhile notLineSep after))
-                                      (line-1, column)
-                                        
-            let range = getRange node
-            let before, _ = takeAroundPos "" source (range.StartLine, range.StartColumn)
-            let _, after = takeAroundPos "" source (range.EndLine, range.EndColumn)
 
-            (Seq.fold (+) "" (Seq.map string before)) + replacementText + (Seq.fold (+) "" (Seq.map string after))
-
-            
         let rec processPairs modifiedSource remainingNodeTextPairs =
             match remainingNodeTextPairs with
                 | [] -> modifiedSource
-                | p::ps -> processPairs (replaceOne modifiedSource p) ps
+                | (n,t)::ps -> processPairs (replaceRange modifiedSource (getRange n, t)) ps
 
         processPairs source sortedPairs
 
 
-    let AddChild (source : string) (node : Ast.AstNode) (childText : string) separator =
-        source
+    let AddChild (source : string) (node : Ast.AstNode) (index : int) (childText : string) =
+        let childrenOption = Ast.GetChildren node
+        let children = if childrenOption.IsSome then childrenOption.Value else raise InvalidNode
+        let range =
+            if index >= children.Length then getRange(children.[children.Length-1]).EndRange
+            else getRange(children.[index]).StartRange
+
+        replaceRange source (range, childText)
