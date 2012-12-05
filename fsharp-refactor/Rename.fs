d2d69194be78e8ade3fca62ea6c58da211f97c4a
Lewis Brown <lewix7@hotmail.com>
2012-12-05 16:50:48 +0000

    First implementation of makeScopeTree
    Works for a simple let expression. Most likely not for mutually
    recursive functions with let rec ... and ...
    
    Still needs implementation of GetDeclarations.

--- fsharp-refactor/Rename.fs
+++ fsharp-refactor/Rename.fs
@@ -1,12 +1,26 @@
 module FSharpRefactor.Refactorings.Rename
 
 open Microsoft.FSharp.Compiler.Range
+open Microsoft.FSharp.Compiler.Ast
 open FSharpRefactor.Engine.Ast
+open FSharpRefactor.Engine.CodeAnalysis
 
 type ScopeTree =
-    | Declaration of string * range * ScopeTree list
+    | Declaration of (string * range) list * ScopeTree list
     | Usage of string * range
-    | Empty
 
-// Returns a scope tree rooted at the first declaration in tree or empty if there are none
-let makeScopeTree (tree : Ast.AstNode) = Empty
+let GetDeclarations p = ["a", mkRange "" (mkPos 1 1) (mkPos 1 1)]
+
+//TODO: mutually recursive functions with and
+let rec makeScopeTree (tree : Ast.AstNode) =
+    match tree with
+        | Ast.AstNode.Expression(SynExpr.LetOrUse(_,_,[b],e,_)) ->
+            let l1,v =
+                match b with
+                    | SynBinding.Binding(_,_,_,_,_,_,_,p,_,e,_,_) ->
+                        (makeScopeTree (Ast.AstNode.Expression e), GetDeclarations p)
+            let l2 = makeScopeTree (Ast.AstNode.Expression e)
+            Declaration(v,l2)::l1
+        | Ast.Children(cs) -> List.concat (Seq.map makeScopeTree cs)
+        | CodeAnalysis.Usage(text, range) -> [Usage(text, range)]
+        | _ -> []
