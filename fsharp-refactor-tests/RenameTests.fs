b044d0560f5570e2cd919dd717064bca5a35b6df
Lewis Brown <lewix7@hotmail.com>
2012-12-06 14:16:48 +0000

    Add another test for makeScopeTree

--- fsharp-refactor-tests/RenameTests.fs
+++ fsharp-refactor-tests/RenameTests.fs
@@ -15,4 +15,14 @@ type ScopeTreeModule() =
 
         match scopeTree with
             | [ScopeTree.Declaration(["a",_],[ScopeTree.Usage("a",_)])] -> ()
-            | _ -> Assert.Fail("The scope tree for 'let a = 1 in a' was incorrect: " + (sprintf "%A" scopeTree))
+            | _ -> Assert.Fail("The scope tree for 'let a = 1 in a' was incorrect:\n" + (sprintf "%A" scopeTree))
+
+    [<Test>]
+    member this.``Creates a scope tree for a more elaborate sequence of let statements``() =
+        let source = "let a =\n  let b = 1\n  let c = 2 + b + b\n  let d = 1\n  b+c\nlet b = 3+a"
+        let rootNode = Ast.Parse source
+        let scopeTree = makeScopeTree (rootNode.Value)
+
+        match scopeTree with
+            | [ScopeTree.Declaration(["a",_],[ScopeTree.Usage("a",_)])] -> ()
+            | _ -> Assert.Fail("The scope tree for 'let a = 1 in a' was incorrect:\n" + (sprintf "%A" scopeTree))
