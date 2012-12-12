a309d6cd6e5b76a03bdede9e06b918c7ceb7498c
Lewis Brown <lewix7@hotmail.com>
2012-12-12 16:03:19 +0000

    Find expression at range

--- fsharp-refactor/ExtractFunction.fs
+++ fsharp-refactor/ExtractFunction.fs
@@ -1,9 +1,26 @@
 module FSharpRefactor.Refactorings.ExtractFunction
 
 open Microsoft.FSharp.Compiler.Range
+open Microsoft.FSharp.Compiler.Ast
 open FSharpRefactor.Engine.Ast
 
-let findExpressionAtRange (tree : Ast.AstNode) range = tree
+let rec findNodesWithRange range (tree : Ast.AstNode) =
+    let nodeRange = Ast.GetRange(tree)
+    let remainingRanges =
+        match tree with
+            | Ast.Children cs -> List.concat (Seq.map (findNodesWithRange range) cs)
+            | _ -> []
+    if Option.isSome nodeRange && range = nodeRange.Value
+    then tree::remainingRanges else remainingRanges
+
+
+let findExpressionAtRange range (tree : Ast.AstNode)  =
+    let nodesWithRange = findNodesWithRange range tree
+    let isExpression node =
+        match node with
+            | Ast.AstNode.Expression _ -> true
+            | _ -> false
+    List.find isExpression nodesWithRange
 
 let CanExtractFunction (tree : Ast.AstNode) (expressionRange : range) (functionName : string) = true
 
