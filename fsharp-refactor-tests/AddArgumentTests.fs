1b20052fd90174ad6b7c7f94af97bdb1a02b5bf2
Lewis Brown <lewix7@hotmail.com>
2012-12-29 15:16:59 +0100

    Test and stub for adding an argument to a binding

--- fsharp-refactor-tests/AddArgumentTests.fs
+++ fsharp-refactor-tests/AddArgumentTests.fs
@@ -1,4 +1,17 @@
 namespace FSharpRefactor.Tests.AddArgumentTests
 
 open NUnit.Framework
+open Microsoft.FSharp.Compiler.Range
+open FSharpRefactor.Engine.Ast
+open FSharpRefactor.Refactorings.AddArgument
 
+[<TestFixture>]
+type AddArgumentModule() =
+    [<Test>]
+    member this.``Can add an argument to a binding``() =
+        let source = "let f a b = a+b"
+        let tree = (Ast.Parse source).Value
+        let bindingRange = mkRange "/home/lewis/test.fs" (mkPos 1 4) (mkPos 1 15)
+        let expected = "let f a b c = a + b"
+
+        Assert.AreEqual(expected, AddArgumentToBinding source tree bindingRange "c")
--- fsharp-refactor/AddArgument.fs
+++ fsharp-refactor/AddArgument.fs
@@ -1,5 +1,8 @@
-module FsharpRefactor.Refactorings.AddArgument
+module FSharpRefactor.Refactorings.AddArgument
 
 open Microsoft.FSharp.Compiler.Range
+open FSharpRefactor.Engine.Ast
+
+let AddArgumentToBinding source (tree : Ast.AstNode) (bindingRange : range) (argumentName : string) = source
 
 let AddArgument source (bindingRange : range) (argumentName : string) (defaultValue : string) = source
