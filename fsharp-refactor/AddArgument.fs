159e764c258d96ff9049de7876df2de68bfd8b76
Lewis Brown <lewix7@hotmail.com>
2012-12-30 22:09:10 +0100

    FindFunctionCalls

--- fsharp-refactor/AddArgument.fs
+++ fsharp-refactor/AddArgument.fs
@@ -5,6 +5,7 @@ open Microsoft.FSharp.Compiler.Ast
 open Microsoft.FSharp.Compiler.Range
 open FSharpRefactor.Engine.Ast
 open FSharpRefactor.Engine.CodeAnalysis.RangeAnalysis
+open FSharpRefactor.Engine.CodeAnalysis.ScopeAnalysis
 open FSharpRefactor.Engine.RefactoringWorkflow
 
 let AddArgumentToBinding source (tree : Ast.AstNode) (bindingRange : range) (argumentName : string) =
@@ -30,7 +31,43 @@ let AddArgumentToFunctionCall source (tree : Ast.AstNode) (callRange : range) (a
         yield (argRange.StartRange, argument + " ")
     }
 
-let FindFunctionCalls source (tree : Ast.AstNode) (definitionRange : range) = []    
-
+let FindFunctionCalls source (tree : Ast.AstNode) (declarationRange : range) (functionName : string) =
+    let isDeclarationOfFunction scopeTree =
+        match scopeTree with
+            | Declaration(is,ts) ->
+                List.exists (fun (n,r) -> n = functionName) is
+            | _ -> false
+    let isApp node =
+        match node with
+            | Ast.AstNode.Expression(SynExpr.App(_,_,_,_,_)) -> true
+            | _ -> false
+    let rangeIfUsageOfFunction scopeTree =
+        match scopeTree with
+            | Usage(n,r) -> if n = functionName then Some r else None
+            | _ -> None
+            
+    let findAppAroundRange (functionRange : range) =
+        let rec tryFindAppAroundRange tree =
+            match tree with
+                | Ast.AstNode.Expression(SynExpr.App(_,_,f,e,r) as app) ->
+                    if rangeContainsRange r functionRange then
+                        let nestedApp = List.tryPick tryFindAppAroundRange
+                                                     [Ast.AstNode.Expression e; Ast.AstNode.Expression f]
+                        if Option.isSome nestedApp then nestedApp else Some app
+                    else None
+                | Ast.Children cs -> List.tryPick tryFindAppAroundRange cs
+                | _ -> None
+        let app = tryFindAppAroundRange tree
+        if Option.isSome app then app.Value
+        else raise (new Collections.Generic.KeyNotFoundException("Not such node"))
+    
+    makeScopeTrees tree
+    |> List.collect ListNodes
+    |> List.find isDeclarationOfFunction
+    |> ListNodes
+    |> List.choose rangeIfUsageOfFunction
+    |> List.map findAppAroundRange
+    |> List.map Ast.AstNode.Expression
+    
+    
 let AddArgument source (bindingRange : range) (argumentName : string) (defaultValue : string) = source
-        
