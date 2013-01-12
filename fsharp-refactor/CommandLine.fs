d49cbfaf1f7b6387422d130cd4394464ed8b0415
Lewis Brown <lewix7@hotmail.com>
2013-01-12 17:58:47 +0100

    Improved command line interface

--- fsharp-refactor/CommandLine.fs
+++ fsharp-refactor/CommandLine.fs
@@ -7,65 +7,133 @@ open Mono.Options
 open Microsoft.FSharp.Compiler.Range
 open FSharpRefactor.Engine.Ast
 open FSharpRefactor.Engine.CodeAnalysis.RangeAnalysis
+open FSharpRefactor.Engine.RefactoringWorkflow
 open FSharpRefactor.Refactorings.Rename
 open FSharpRefactor.Refactorings.ExtractFunction
 open FSharpRefactor.Refactorings.AddArgument
-    
+
+let getSource filename =
+    match filename with
+        | Some f -> File.ReadAllText f
+//TODO: read code from stdout
+
 let Rename filename position newName =
-    let source = File.ReadAllText filename
+    let source = getSource filename
     let tree = (Ast.Parse source).Value
     let declarationIdentifier = FindDeclarationIdentifier source position
     DoRename source tree declarationIdentifier newName
 
-let ExtractFunction filename expressionRange functionName =
-    let source = File.ReadAllText filename
+let ExtractFunction filename (startPosition,endPosition) functionName =
+    let source = getSource filename
     let tree = (Ast.Parse source).Value
+    let expressionRange = mkRange "/home/lewis/test.fs" startPosition endPosition
     let inScopeTree = (DefaultInScopeTree source tree expressionRange).Value
     DoExtractFunction source tree inScopeTree expressionRange functionName
 
 let AddArgument filename position argumentName defaultValue =
-    let source = File.ReadAllText filename
+    let source = getSource filename
     let tree = (Ast.Parse source).Value
     let bindingRange = (DefaultBindingRange source tree position).Value
     AddArgument source tree bindingRange argumentName defaultValue
 
-let tryParsePos (positionString : string) =
+let printUsage () =
+    printfn "Usage:"
+    printfn "  rename <position> <new_name> [<filename>]"
+    printfn "  extract-function  <expression_range> <function_name> [<filename>]"
+    printfn "  add-argument <position> <argument_name> <default_value> [<filename>]"
+    printfn ""
+    printfn "Options:"
+    printfn "  -h, --help                           Display this message and exit"
+    printfn "  -i [SUFFIX], --in-place=[SUFFIX]     Modify the input file in-place (makes backup if extension supplied)"
+    printfn "  -o FILENAME, --output-file=FILENAME  Write result to FILENAME"
+    
+
+exception ArgumentException of string
+
+let parsePos (positionString : string) =
     let m = Regex.Match(positionString, "([0-9]+):([0-9]+)")
     if m.Success then
         let line = Int32.Parse m.Groups.[1].Value
         let column = Int32.Parse m.Groups.[2].Value
-        Some (mkPos line column)
-    else None
+        mkPos line column
+    else raise (ArgumentException (sprintf "%s is not a valid position" positionString))
 
+let parseRenameArguments (args : string list) =
+    match List.length args with
+        | 0 | 1 -> raise (ArgumentException "Too few arguments")
+        | 2 -> (parsePos args.[0], args.[1], None)
+        | 3 -> (parsePos args.[0], args.[1], Some args.[2])
+        | _ -> raise (ArgumentException "Too many arguments")
+
+let parseExtractFunctionArguments (args : string list) =
+    match List.length args with
+        | 0 | 1 | 2 -> raise (ArgumentException "Too few arguments")
+        | 3 -> ((parsePos args.[0], parsePos args.[1]), args.[2], None)
+        | 4 -> ((parsePos args.[0], parsePos args.[1]), args.[2], Some args.[3])
+        | _ -> raise (ArgumentException "Too many arguments")
+
+let parseAddArgumentArguments (args : string list) =
+    match List.length args with
+        | 0 | 1 | 2 -> raise (ArgumentException "Too few arguments")
+        | 3 -> (parsePos args.[0], args.[1], args.[2], None)
+        | 4 -> (parsePos args.[0], args.[1], args.[2], Some args.[3])
+        | _ -> raise (ArgumentException "Too many arguments")
+
+let refactorWithArguments args =
+    match args with
+        | "rename"::rest ->
+            let position, newName, filename =
+                parseRenameArguments rest
+            Rename filename position newName
+        | "extract-function"::rest ->
+            let expressionRange, functionName, filename =
+                parseExtractFunctionArguments rest
+            ExtractFunction filename expressionRange functionName
+        | "add-argument"::rest ->
+            let position, argumentName, defaultValue, filename =
+                parseAddArgumentArguments rest
+            AddArgument filename position argumentName defaultValue
+        | name::_ -> raise (ArgumentException (sprintf "%s is not a valid refactoring name" name))
+        | [] -> raise (ArgumentException "Too few arguments")
+
+type Options() =
+    member this.Help = ref false
+    member this.InPlace = ref false
+    member this.OutputFile = ref None
+//TODO: Handle KeyNotFoundException sensible
 [<EntryPoint>]
 let main(args : string[]) =
+    let options = new Options()
     let optionSet = new OptionSet()
-    let printUsage () =
-        printfn "Usage:"
-        printfn "  rename <filename> <position> <new_name>"
-        printfn "  extract-function <filename> <expression_range> <function_name>"
-        printfn "  add-argument <filename> <position> <argument_name> <default_value>"
-        
-    let extra = optionSet.Parse(args)
+    //TODO: get the argument passed in with -i or -o
+    optionSet.Add("h|help", fun _ -> options.Help := true) |> ignore
+    optionSet.Add("i|in-place", fun s -> options.InPlace := true) |> ignore
+    optionSet.Add("o|output-file", fun f -> options.OutputFile := Some f) |> ignore
+    let extra = Seq.toList (optionSet.Parse(args))
 
-    match extra.[0] with
-        | "rename" ->
-            let position = tryParsePos extra.[2]
-            if Option.isNone position then printUsage ()
-            else
-                printfn "%s" (Rename extra.[1] position.Value extra.[3])
-        | "extract-function" ->
-            let startPos = tryParsePos extra.[2]
-            let endPos = tryParsePos extra.[3]
-            if Option.isNone startPos && Option.isNone endPos then printUsage ()
-            else
-                let range = mkRange "/home/lewis/test.fs" startPos.Value endPos.Value
-                printfn "%s" (ExtractFunction extra.[1] range extra.[4])
-        | "add-argument" ->
-            let position = tryParsePos extra.[2]
-            if Option.isNone position then printUsage ()
-            else
-                printfn "%s" (AddArgument extra.[1] position.Value extra.[3] extra.[4])
-        | _ -> printUsage ()
+    if !options.Help then
+        printUsage ()
+        0
+    else
+        let resultCode =
+            try Some (refactorWithArguments extra) with
+                | ArgumentException message ->
+                    printfn "%s" message
+                    printfn ""
+                    printUsage ()
+                    None
+                | RefactoringFailure message ->
+                    printfn "%s" message
+                    None
 
-    0
+        if Option.isNone resultCode then
+            1
+        else
+            if !options.InPlace then
+                //TODO: write resultCode back to the file after backing it up
+                ()
+            if Option.isSome !options.OutputFile then
+                //TODO: write resultCode to outputfile
+                ()
+            printfn "%s" resultCode.Value
+            0
