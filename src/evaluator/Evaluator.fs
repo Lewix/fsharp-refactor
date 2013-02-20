5a4777773f55e88a860c92faa9395e0dc953f39a
Lewis Brown <lewix7@hotmail.com>
2013-02-20 19:20:17 +0000

    Make a type for refactoring results

--- src/evaluator/Evaluator.fs
+++ src/evaluator/Evaluator.fs
@@ -7,6 +7,20 @@ open FSharpRefactor.Evaluator.GenerationState
 open FSharpRefactor.Evaluator.BehaviourChecker
 open FSharpRefactor.Evaluator.CodeRefactorer
 
+type Status =
+    | Succeeded
+    | Failed
+    | Exception
+
+type RefactoringResult = {
+    changed : bool;
+    status : Status;
+    sourceBefore : string;
+    sourceAfter : string option;
+    refactoring : string;
+    time : DateTime
+    }
+
 let last ls = List.reduceBack (fun _ l -> l) ls
 
 let randomIdent (random : Random) =
@@ -14,38 +28,70 @@ let randomIdent (random : Random) =
     |> generateIdent Int
     |> fst
 
+let tryRefactoring refactoring =
+    try
+        let result = refactoring ()
+        if Option.isSome result then
+            Succeeded, result
+        else
+            Failed, result
+    with
+        | _ -> Exception, None
+
 let evaluateExtractFunction source (random : Random) =
-    randomExtractFunction source (randomIdent random) (random.Next()) (random.Next())
+    let status, sourceAfter =
+        tryRefactoring (fun () -> randomExtractFunction source (randomIdent random) (random.Next()) (random.Next()))
+    { status = status;
+      sourceBefore = source;
+      sourceAfter = sourceAfter;
+      refactoring = "extract-function";
+      time = DateTime.Now;
+      changed = false }
 
 let evaluateAddArgument source (random : Random) =
-    randomAddArgument source (randomIdent random) (random.Next()) (random.Next())
+    let status, sourceAfter =
+        tryRefactoring (fun () -> randomAddArgument source (randomIdent random) (random.Next()) (random.Next()))
+    { status = status;
+      sourceBefore = source;
+      sourceAfter = sourceAfter;
+      refactoring = "add-argument";
+      time = DateTime.Now;
+      changed = false }
 
 let evaluateRename source (random : Random) =
-    randomRename source (randomIdent random) (random.Next())
+    let status, sourceAfter =
+        tryRefactoring (fun () -> randomRename source (randomIdent random) (random.Next()))
+    { status = status;
+      sourceBefore = source;
+      sourceAfter = sourceAfter;
+      refactoring = "rename";
+      time = DateTime.Now;
+      changed = false }
 
 let evaluateRefactoring refactoring =
     let entryPoint = "f"
     let codeTemplate, code = generateEntryPoint entryPoint defaultState
     let random = new Random()
     let refactoringResult = refactoring code random
+    let changed =
+        if Option.isSome refactoringResult.sourceAfter then
+            BehaviourHasChanged entryPoint refactoringResult.sourceBefore refactoringResult.sourceAfter.Value
+        else
+            false
 
-    if Option.isSome refactoringResult then
-        let before = codeTemplate code
-        let after = codeTemplate refactoringResult.Value
-        Some(BehaviourHasChanged entryPoint before after, before, after)
-    else
-        None
+    { refactoringResult with changed = changed }
 
 let evaluateRefactorings refactoring iterations (resultsFile : string) =
     let evaluations = Seq.init iterations (fun i -> evaluateRefactoring refactoring)
     let fileWriter = new StreamWriter(resultsFile)
+    ignore (fprintfn fileWriter "status,before,after,refactoring,time")
 
     let writeResultLine result =
-        if Option.isSome result then
-            let changed, before, after = result.Value
-            ignore (fprintfn fileWriter "%A,%A,%A" changed before after)
-        else
-            ignore (fprintfn fileWriter "failed,,")
+        fprintfn fileWriter "%A,%A,%A,%A,%A" result.status
+                                             result.sourceBefore
+                                             result.sourceAfter
+                                             result.refactoring
+                                             result.time
         fileWriter.Flush()
 
     Seq.iter writeResultLine evaluations
