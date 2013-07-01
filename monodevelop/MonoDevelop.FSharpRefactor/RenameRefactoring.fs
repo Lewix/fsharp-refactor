ec4bd8216c954315985672ac137a664d6d7228a4
Lewis Brown <lewix7@hotmail.com>
2013-07-01 17:38:16 +0100

    Partial implementation of Rename refactoring's binding
    Currently always valid for F# files and always renames to "testName".

---
+++ monodevelop/MonoDevelop.FSharpRefactor/RenameRefactoring.fs
@@ -0,0 +1,50 @@
+namespace MonoDevelop.FSharpRefactor.Rename
+
+open System
+open MonoDevelop.Components.Commands
+open MonoDevelop.Refactoring
+open MonoDevelop.Ide
+open MonoDevelop.Ide.Gui
+open MonoDevelop.Ide.Gui.Content
+open MonoDevelop.Projects.Text
+open Mono.TextEditor
+open Microsoft.FSharp.Compiler.Range
+
+open FSharpRefactor.Engine.Ast
+open FSharpRefactor.Engine.CodeAnalysis.RangeAnalysis
+open FSharpRefactor.Engine.CodeAnalysis.ScopeAnalysis
+open FSharpRefactor.Refactorings.Rename
+
+type RenameRefactoring() as self =
+    inherit RefactoringOperation()
+
+    let rename (source:string) (position:pos) (newName:string) =
+        let tree = (Ast.Parse source).Value
+        let identifier = FindIdentifier source position
+        if Option.isSome identifier then
+            let declarationIdentifier =
+                TryFindIdentifierDeclaration (makeScopeTrees tree) identifier.Value
+            if Option.isSome declarationIdentifier then
+                DoRename source tree declarationIdentifier.Value newName
+            else
+                source
+        else
+            source
+
+    do
+        self.Name <- "Rename (F#)"
+    override self.IsValid(options) =
+        options.MimeType = "text/x-fsharp"
+    override self.PerformChanges(options, properties) =
+        let doc = options.Document
+        let wholeFileChange = new TextReplaceChange()
+        let fileName = options.Document.FileName.ToString()
+        wholeFileChange.FileName <- fileName
+        wholeFileChange.Offset <- 0
+        let textFile = doc.GetContent<ITextFile>()
+        wholeFileChange.RemovedChars <- textFile.Length
+        let position = mkPos options.Location.Line (options.Location.Column-1)
+        wholeFileChange.InsertedText <-
+            rename textFile.Text position "testName" 
+
+        Collections.Generic.List<Change>([wholeFileChange :> Change])
