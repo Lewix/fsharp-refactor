7a76b60c2a7513a1b96e6b3c33fb605ff51f5ac9
Lewis Brown <lewix7@hotmail.com>
2013-08-11 15:51:07 +0200

    Remove ModuleScopeTrees

--- FSharpRefactorTests/ProjectTests.fs
+++ FSharpRefactorTests/ProjectTests.fs
@@ -7,7 +7,6 @@ open Microsoft.FSharp.Compiler.Range
 open FSharpRefactor.Engine
 open FSharpRefactor.Engine.Ast
 open FSharpRefactor.Engine.ScopeAnalysis
-open FSharpRefactor.Engine.ModuleScopeTree
 
 [<TestFixture>]
 type ModuleScopeTreeModule() =
@@ -42,79 +41,7 @@ type ModuleScopeTreeModule() =
     member this.DeleteFiles () =
         List.map File.Delete files
         |> ignore
-        
-    [<Test>]
-    member this.``Can create the scope trees for a single module declaration``() =
-        let source = "module TestModule1\n  let TopLevelFunction1 a = 1+a\n  let TopLevelFunction2 = 2*(TopLevelFunction1b)"
-        let moduleScopeTrees = makeModuleScopeTrees (singleFileProject source)
-        
-        match moduleScopeTrees with
-            | [Declaration((("TestModule1", r), ["TestModule1.TopLevelFunction1",_;"TestModule1.TopLevelFunction2",_]),[])] when r = mkRange (Path.GetFullPath "test.fs") (mkPos 1 0) (mkPos 3 48) -> ()
-            | _ -> Assert.Fail(incorrectScopeTrees source moduleScopeTrees)
 
-    [<Test>]
-    member this.``Can create the scope trees for a module declaration with nested modules``() =
-        let source = "module TestModule1\n  let TopLevelValue1 = 1\n  module TestModule2 =\n    let TopLevelValue2 = 2"
-        let moduleScopeTrees = makeModuleScopeTrees (singleFileProject source)
-        
-        match moduleScopeTrees with
-            | [Declaration((("TestModule1", _), ["TestModule1.TopLevelValue1",_;"TestModule1.TestModule2.TopLevelValue2",_]),  [])] ->  ()
-            | _ -> Assert.Fail(incorrectScopeTrees source moduleScopeTrees)
-            
-    [<Test>]
-    member this.``Can create the scope trees for namespaces with nested modules``() =
-        let source = "namespace Test1\n\nmodule TestModule1 =\n  let TopLevelValue1 = 1\n\nnamespace Test2\n\nmodule TestModule2 =\n  let TopLevelValue2 = 2"
-        let moduleScopeTrees = makeModuleScopeTrees (singleFileProject source)
-        
-        match moduleScopeTrees with
-            | [Declaration((("Test1.TestModule1", _), ["Test1.TestModule1.TopLevelValue1",_]),
-                [Declaration((("Test2.TestModule2", _), ["Test2.TestModule2.TopLevelValue2",_]),[])])] -> ()
-            | _ -> Assert.Fail(incorrectScopeTrees source moduleScopeTrees)
-            
-    [<Test>]
-    member this.``Can create the scope trees for a namespace with nested modules``() =
-        let source = "namespace Test\n\nmodule TestModule1 = \n  let TopLevelValue1 = 1\n\nmodule TestModule2 =\n  let TopLevelValue2 = 2"
-        let moduleScopeTrees = makeModuleScopeTrees (singleFileProject source)
-        
-        match moduleScopeTrees with
-            | [Declaration((("Test.TestModule1",_), ["Test.TestModule1.TopLevelValue1",_]),
-                [Declaration((("Test.TestModule2",_), ["Test.TestModule2.TopLevelValue2",_]),[])])] -> ()
-            | _ -> Assert.Fail(incorrectScopeTrees source moduleScopeTrees)
-            
-    [<Test>]
-    member this.``Can create the entire scope trees for modules over two files``() =
-        let files = [files.[0]; files.[1]]
-        let project = new Project(files.[0], List.zip files [None; None] |> List.toArray)
-        let moduleScopeTrees = makeModuleScopeTrees project
-        
-        match moduleScopeTrees with
-            | [Declaration((("Test.TestModule1",_), ["Test.TestModule1.TopLevelFunction1",_; "Test.TestModule1.TopLevelFunction2",_]),
-                [Declaration((("Test.TestModule2",_), ["Test.TestModule2.TopLevelFunction1",_]),
-                    [Declaration((("Test.TestModule3",_), ["Test.TestModule3.TopLevelFunction3",_]),[]);
-                     Usage("Test.TestModule1.TopLevelFunction1",_);
-                     Usage("Test.TestModule1.TopLevelFunction2",_)])])] -> ()
-            | _ -> Assert.Fail(incorrectScopeTrees project.CurrentFileContents moduleScopeTrees)
-            
-    [<Test>]
-    member this.``Can create the usages in the scope trees for opened modules``() =
-        let source =
-            String.concat "\n" ["namespace Test";
-                                "module TestModule1 =";
-                                "  let TopLevelValue1 = 1";
-                                "  let TopLevelValue2 = 2";
-                                "  let TopLevelFunction3 a = 2*a";
-                                "module TestModule2 =";
-                                "  open TestModule1";
-                                "  let f = TestModule1.TopLevelValue2 + (TopLevelFunction3 2)"]
-        let moduleScopeTrees = makeModuleScopeTrees (singleFileProject source)
-                
-        match moduleScopeTrees with
-            | [Declaration((("Test.TestModule1",_),["Test.TestModule1.TopLevelValue1",_;"Test.TestModule1.TopLevelValue2",_;"Test.TestModule1.TopLevelFunction3",_]),
-                [Declaration((("Test.TestModule2",_),["Test.TestModule2.f",_]),[]);
-                 Usage("Test.TestModule1.TopLevelValue2",r);
-                 Usage("Test.TestModule1.TopLevelFunction3",_)])] when r = mkRange (Path.GetFullPath "test.fs") (mkPos 8 10) (mkPos 8 36) -> ()
-            | _ -> Assert.Fail(incorrectScopeTrees source moduleScopeTrees)
-            
     [<Test>]
     member this.``Can get a list of modules from a piece of source code``() =
         let filename = Path.GetFullPath "test.fs"
