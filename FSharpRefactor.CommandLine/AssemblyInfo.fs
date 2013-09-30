8162630b261c2b57b38192d558d27f7468a4bf88
Lewis Brown <lewix7@hotmail.com>
2013-09-30 23:07:47 +0200

    Add AssemblyInfo.cs files
    So that Windows users can build the project without needing autotools.

---
+++ FSharpRefactor.CommandLine/AssemblyInfo.fs
@@ -0,0 +1,11 @@
+module FSharpRefactor.CommandLine.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyVersion("0.1.0.0")>]
+[<assembly: AssemblyTitle("FSharpRefactor.CommandLine")>]
+[<assembly: AssemblyDescription("Command line interface for fsharp-refactor")>]
+
+()
+
